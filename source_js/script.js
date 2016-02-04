d3694dec5157065bc1fc76b2870c5bcbab81dc4a
xren11 <xianliren@gmail.com>
2016-02-04 01:11:28 -0600

    update1

--- source_js/script.js
+++ source_js/script.js
@@ -14,3 +14,24 @@ function highlightThis(event) {
     alert(this.className);
     this.style.backgroundColor=backgroundColor;
 }
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+            $("nav").addClass("shrink");
+        } else {
+            //$("#navbar2").fadeIn();
+            $("nav").removeClass("shrink");
+        }
+    });
+    $("#icon1").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#icon1-scroll").offset().top
+        }, 500);
+    });
+    $("#icon2").click(function() {
+        $('html, body').animate({
+            //var x = $("#icon2-scroll").offset().top
+            scrollTop: $("#icon2-scroll").offset().top
+        }, 1000);
+    });
+});
